52657fb9cc8a60ac4fd12cb96d03013da0894681
Pablo Mendes Faria <pablomendesfaria@gmail.com>
2025-01-06 02:49:19 -0300

    creating a __init__ file in app module, and adjusting the correct folder for output file on scraper.py

--- app/scraper.py
+++ app/scraper.py
@@ -137,9 +137,9 @@ async def main():
         scraped_data = pd.DataFrame(valid_results, columns=columns)  # Convert the list to a dataframe
         scraped_data.columns = ["Year", "Country", "Goods", "ExportWeightTon", "ExportValueKUSD", "ImportWeightTon", "ImportValueKUSD", "BalanceOfTradeKUSD"]  # Rename the columns
         scraped_data.insert(0, "ScrapeDatetime", scrape_datetime)  # Insert the scrape datetime column
-        scraped_data.to_csv(f"scrape/{filename}.csv", encoding="utf-8", lineterminator="\n", quotechar='"', quoting=csv.QUOTE_ALL, index=False)  # Save the dataframe to a csv file?
+        scraped_data.to_csv(f"data/{filename}.csv", encoding="utf-8", lineterminator="\n", quotechar='"', quoting=csv.QUOTE_ALL, index=False)  # Save the dataframe to a csv file?
 
-        print(f"Data saved in {filename}")
+        print(f"Data saved in data/{filename}.csv")
     else:
         print("No data was collected.")
 
